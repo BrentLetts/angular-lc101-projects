d43579ea2e3a13bdd1e1bb73042187df238cae79
Brent Letts <brent.letts@gmail.com>
2019-12-23 20:29:41 -0600

    additions for the lesson, also added in a delete for items in the movie list

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
    movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
-
    constructor() { }
 
    ngOnInit() {
+   }
+
+   addMovie(newTitle: string){
+      let errorMessage = '';
+      if(this.movies.includes(newTitle)){
+         errorMessage = `${newTitle} is already in the list`;
+      }else if(newTitle === ""){
+         errorMessage = "Please enter a title";
+      }else{
+         this.movies.push(newTitle);
+      }
+      return errorMessage;
+   }
+
+   removeFromList(title: string){
+      let movieIndex = this.movies.indexOf(title);
+      let array = this.movies;
+      this.movies.splice(movieIndex);
    }
 }
